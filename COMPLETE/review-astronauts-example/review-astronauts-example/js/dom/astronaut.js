04c3ebf29c09569ae599a7f257fce67708bcc50f
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2024-02-18 14:01:34 -0700

    ok

---
+++ COMPLETE/review-astronauts-example/review-astronauts-example/js/dom/astronaut.js
@@ -0,0 +1,38 @@
+/*
+HTML of an astronaut list item.
+replace the instances that have "THIS FORMAT HERE" with the astronautData.
+
+<li href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+        <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">    
+        <h5 class="mb-1">ASTRONAUT NAME (ASTRONAUT STATUS HERE)</h5>
+        <small class="float-end">born DATE OF BIRTH HERE</small>
+    </div>
+    <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>    
+    <p class="mb-1">ASTRONAUT BIO HERE </p>
+</li>
+*/
+
+const renderAstronautListItem = (data, listElement) => {
+    let {
+        agency,
+        bio,
+        date_of_birth,
+        name,
+        nationality,
+        profile_image_thumbnail,
+        status
+    } = data
+
+    listElement.innerHTML += `<li href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+            <img src="${profile_image_thumbnail}" class="rounded float-start" alt=""">    
+            <h5 class="mb-1">${name} (${status.name})</h5>
+            <small class="float-end">born ${date_of_birth}</small>
+        </div>
+        <small>${nationality} (${agency.abbrev})</small>    
+        <p class="mb-1">${bio} </p>
+    </li>`
+}
+
+export {renderAstronautListItem}
---
+++ nextjs-event-state-jsx-conditionals-START/state-events-conditionals-example/pages/state-event-condition/components/SimpleListItem.js
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function SimpleLisItem( {text}) {
+ return <ListItem>
+<ListItemText>
+  <Typography variant="p" component="div">
+    {text}
+  </Typography>
+</ListItemText>
+</ListItem>
+
+}
